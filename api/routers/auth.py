5fba4f5a22f01b1d3444f5d9277f45e8cbcf86a3
washizawa haruya (JSL) <129074090+washizawa83@users.noreply.github.com>
2024-09-08 17:01:04 +0900

    認証のエンドポイント修正・追加 (#20)

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -1,13 +1,15 @@
 from datetime import timedelta
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.schemas.auth as auth_schema
+import api.schemas.user as user_schema
 import api.cruds.user as user_crud
 import api.cruds.auth as auth_crud
 import api.utils.env as env
 from api.db import get_db
+from api.service.auth import get_active_user_permission
 
 
 router = APIRouter()
@@ -23,8 +25,8 @@ async def signup(
     return new_user is not None
 
 
-@router.post('/verification', response_model=str | None)
-async def verification(
+@router.post('/signup-verify', response_model=str | None)
+async def signup_verify(
     body: auth_schema.Verification, db: AsyncSession = Depends(get_db)
 ):
     await auth_crud.verify_user_code(db, body)
@@ -32,6 +34,21 @@ async def verification(
     await auth_crud.delete_verification(db, body)
     active_user = await user_crud.update_user_state(db, body)
     return active_user.username
+
+
+@router.get('/verify', response_model=None)
+async def verify(
+    db: AsyncSession = Depends(get_db),
+    user: user_schema.User = Depends(get_active_user_permission),
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+    if not user:
+        raise credentials_exception
+    return None
 
 
 @router.post('/login')
