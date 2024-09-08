b49ba6f9c73d52741876b6fe81e1529b4e2c1ea4
washizawa83 <washizawa83@gmail.com>
2024-09-08 17:00:27 +0900

    認証のエンドポイント修正・追加

--- api/service/auth.py
+++ api/service/auth.py
@@ -36,5 +36,5 @@ async def get_active_user_permission(
 ):
     user = await get_user(db, token_data.user_id)
     if not user.is_active:
-        raise HTTPException(status_code=400, detail='Inactive user')
+        raise HTTPException(status_code=401, detail='Inactive user')
     return user
